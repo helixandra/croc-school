0fe830051b3a5fd54e0a806685f51b7da2f2221a
Alexandra <alexa.min31@gmail.com>
2022-10-24 22:17:13 +0300

    change output text

--- src/main/java/ru/croc/task1/Task1.java
+++ src/main/java/ru/croc/task1/Task1.java
@@ -1,13 +1,9 @@
 package ru.croc.task1;
 
-/**
- * Hello world!
- *
- */
 public class Task1
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello, Java ^^" );
     }
 }
